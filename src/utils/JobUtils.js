87246f37f1b975e661c210d66be04135275c6dd0
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2021-04-26 16:26:45 -0300

    finished class three

---
+++ src/utils/JobUtils.js
@@ -0,0 +1,25 @@
+module.exports = {
+    remainingDays(job) {
+        // calculo no tempo restante
+        let remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+        const isNaN = remainingDays === 'NaN'
+
+        if(isNaN) {
+            remainingDays = '0'
+        }
+    
+        const createdDate = new Date(job.createdAt)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDateInMs = createdDate.setDate(dueDay)
+    
+        const timeDiffInMs = dueDateInMs - Date.now()
+        // transformar ms em dias
+        const dayInMs = 1000 * 60 * 60 * 24
+        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+    
+        // restam n dias
+        return dayDiff
+    },
+
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
+}
